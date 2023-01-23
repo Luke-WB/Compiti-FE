69d3ad8ad43a4f415bf68006ab5e49dc1a68d822
Luca Scomazzon <scom.luca@gmail.com>
2023-01-23 18:03:51 +0100

    Aggiungo nono compito

--- U2-W7 23-1/form.js
+++ U2-W7 23-1/form.js
@@ -1,7 +1,22 @@
-let dati = (event) => {
-  let nome = document.getElementById("name");
-  let cognome = document.getElementById("surname");
-  let data = document.getElementById("dataNascita");
-  let table = document.querySelector("table");
-  table.innerHTML += `<tr><td>${nome.value + " " + cognome.value + " " + data.value}</td></tr>`;
+const nome = document.getElementById("name");
+const cognome = document.getElementById("surname");
+const data = document.getElementById("data");
+
+const Utente = function (nome, cognome, data) {
+  this.nome = nome;
+  this.cognome = cognome;
+  this.data = data;
+};
+
+const utente = (event) => {
+  const table = document.querySelector("table");
+  table.innerHTML += `<tr><td>${nome.value + " " + cognome.value + " " + data.value}
+  <i class="fa-solid fa-trash-can" onclick="rimozione(event)"></i></td></tr>`;
+  const giova = new Utente(nome.value, cognome.value, data.value);
+  console.log(giova);
+};
+
+const rimozione = (e) => {
+  e.stopPropagation();
+  e.target.parentElement.parentElement.remove();
 };
