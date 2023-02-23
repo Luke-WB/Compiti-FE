404253bd81d489d4b5acce5b0ee99af82a4d0b59
Luca Scomazzon <scom.luca@gmail.com>
2023-02-23 09:23:20 +0100

    Aggiungo undicesimo compito

---
+++ U3-W3 22-2/index.js
@@ -0,0 +1,2 @@
+let x = "Marco";
+x = 20;
---
+++ U3-W3 22-2/index.ts
@@ -0,0 +1,9 @@
+let x = "Marco";
+x = 30;
+
+let y: any = 30;
+y = "Marco";
+
+type Union = string | number | boolean;
+
+const tuple: [string, number] = ["Marco", 5];
